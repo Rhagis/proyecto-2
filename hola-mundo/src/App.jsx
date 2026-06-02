3c48b099e5e562d7c33a8abfa0f888aa517c4d5a
Rhagis <pikachu17dual@gmail.com>
2026-06-01 22:57:54 -0300

    c

--- hola-mundo/src/App.jsx
+++ hola-mundo/src/App.jsx
@@ -14,17 +14,14 @@ function App() {
   return (
     <>
       <nav className="nav flex-row justify-content-center gap-3 mb-4 border-bottom border-secondary pb-3 bg-dark shadow-lg">
-        <Link className="nav-link text-light" to='/'>Inicio</Link>
-        <br />
         <Link className="nav-link text-light" to="/lista">Lista de libros</Link>
         <br />
         <Link className="nav-link text-light" to='/libro/añadir'>Añadir Libro</Link>
       </nav>
       <Routes>
-        <Route path='/'></Route>
+        <Route path='/' element={<AñadirLibro></AñadirLibro>}></Route>
         <Route path='/lista' element={<ObtenerDato></ObtenerDato>}></Route>
         <Route path='/lista/libro/:idLibro' element={<ObtenerDatoPorId ></ObtenerDatoPorId>}></Route>
-        <Route path='/libro/añadir' element={<AñadirLibro></AñadirLibro>}></Route>
         <Route path='/lista/libro/editar/:idLibro' element={<EditarLibro></EditarLibro>}></Route>
       </Routes>
       
